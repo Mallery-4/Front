a16c9d6f5c268e03b2e3b5f4590de45d6218569b
Jinyoung Kim <khappy9888@sookmyung.ac.kr>
2023-03-26 12:47:24 +0900

    feature: bottom navigation setting
    custom buttom : meow로 적용

---
+++ Mallery4/app/src/main/java/com/example/mallery4/MypageFragment.kt
@@ -0,0 +1,25 @@
+package com.example.mallery4
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class MypageFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_mypage, container, false)
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() =
+            MypageFragment().apply {
+                arguments = Bundle().apply {}
+            }
+    }
+}
