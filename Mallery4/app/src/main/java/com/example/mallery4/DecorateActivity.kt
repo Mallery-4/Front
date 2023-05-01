9325906c414cad8ae1e6017172c107493cbea583
Jueun Kim <92364973+Jueunkim429@users.noreply.github.com>
2023-05-01 13:56:20 +0900

    [feat] 꾸미기 화면 전환

---
+++ Mallery4/app/src/main/java/com/example/mallery4/DecorateActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mallery4
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class DecorateActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_decorate)
+
+    }
+}
