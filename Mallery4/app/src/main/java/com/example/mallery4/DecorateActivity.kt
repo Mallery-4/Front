dfd7c2686f0cde87f34b1ad6fd6df0171b999dfe
Jueun Kim <92364973+Jueunkim429@users.noreply.github.com>
2023-05-01 14:49:46 +0900

    [feat] 사진 선택 완료

--- Mallery4/app/src/main/java/com/example/mallery4/DecorateActivity.kt
+++ Mallery4/app/src/main/java/com/example/mallery4/DecorateActivity.kt
@@ -1,13 +1,21 @@
 package com.example.mallery4
 
+import android.Manifest.permission.WRITE_EXTERNAL_STORAGE
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
-import android.view.View
+import android.provider.MediaStore
+import android.widget.Button
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import kotlinx.android.synthetic.main.activity_decorate.*
 
 class DecorateActivity: AppCompatActivity() {
+    private val PERMISSION_Album = 101 // 앨범 권한 처리
+    private val REQUEST_STORAGE = 102 // 앨범에서 이미지 가져오기
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_decorate)
@@ -17,6 +25,70 @@ class DecorateActivity: AppCompatActivity() {
             onBackPressed()
         }
 
+        // 앨범 버튼 클릭 리스너 구현
+        val cameraBtn = findViewById<Button>(R.id.camera_btn)
+        cameraBtn.setOnClickListener {
+            // Request write external storage permission
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(WRITE_EXTERNAL_STORAGE),
+                PERMISSION_Album
+            )
+        }
+    }
 
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            PERMISSION_Album -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    permissionGranted(requestCode)
+
+                } else {
+                    permissionDenied(requestCode)
+                }
+            }
+        }
+    }
+
+    private fun permissionGranted(requestCode: Int) {
+        when (requestCode) {
+            PERMISSION_Album -> openGallery()
+        }
+    }
+
+    private fun permissionDenied(requestCode: Int) {
+        when (requestCode) {
+            PERMISSION_Album -> Toast.makeText(
+                this,
+                "저장소 권한을 승인해야 앨범에서 이미지를 불러올 수 있습니다.",
+                Toast.LENGTH_LONG
+            ).show()
+        }
+    }
+
+    private fun openGallery() {
+        val intent = Intent(Intent.ACTION_PICK)
+        MediaStore.Images.Media.CONTENT_TYPE.also { intent.type = it }
+        startActivityForResult(intent, REQUEST_STORAGE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (resultCode == RESULT_OK) {
+            when (requestCode) {
+                REQUEST_STORAGE -> {
+                    data?.data?.let { uri ->
+                        imagePreview.setImageURI(uri)
+                    }
+                }
+            }
+        }
     }
 }
+
