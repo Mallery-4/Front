f460966518f3f47c742f38b38ef6c04d7d0d2105
Jueun Kim <92364973+Jueunkim429@users.noreply.github.com>
2023-05-01 14:16:27 +0900

    [feat] 뒤로 가기 버튼 완료

--- Mallery4/app/src/main/java/com/example/mallery4/DecorateActivity.kt
+++ Mallery4/app/src/main/java/com/example/mallery4/DecorateActivity.kt
@@ -1,12 +1,22 @@
 package com.example.mallery4
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_decorate.*
 
 class DecorateActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_decorate)
+
+        val back = findViewById<ImageView>(R.id.back)
+        back.setOnClickListener {
+            onBackPressed()
+        }
+
 
     }
 }
